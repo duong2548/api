b6131d833b096d22074a745acbddaf38d17946eb
duong2548 <dothuyduong2898@gmail.com>
2018-09-16 21:04:11 +0700

    init project

---
+++ test.php
@@ -0,0 +1,46 @@
+<?php
+
+function connectDB(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "project_bansachonline";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+       return null;
+    }
+    return $conn;
+}
+
+// Đoạn này lấy dữ liệu thô từ client gửi lên
+$raw = file_get_contents('php://input');
+
+// Parse json thành object 
+$jsonObj = json_decode($raw, true);
+
+// Lấy đối tượng connect database 
+$conn = connectDB();
+
+// Check nếu nó ok thì mới xử lý tiếp
+if ($conn) {
+    // Lấy các trường cần thiết từ json object 
+    $id_khach_hang = $jsonObj['id_khach_hang'];
+    $trigia = $jsonObj['trigia'];
+    $trangthai = $jsonObj['trangthai'];
+    $ngaydathang = $jsonObj['ngaydathang'];
+    $ngaygiaohang = $jsonObj['ngaygiaohang'];
+    $ngaydathang = strtotime($ngaydathang);
+    $ngaygiaohang = strtotime($ngaygiaohang);
+    // $ngaydathang = date("Y-m-d H:i:s", $ngaydathang);
+    // $ngaygiaohang = date("Y-m-d H:i:s", $ngaygiaohang);
+
+    $sql = "INSERT INTO dat_hang (id_khach_hang, ngaydathang, trigia, trangthai, ngaygiaohang)
+            VALUES (".$id_khach_hang.",FROM_UNIXTIME(".$ngaydathang."),".$trigia.",".$trangthai.",FROM_UNIXTIME(".$ngaygiaohang."))";
+    if ($conn->query($sql) === TRUE) {
+        echo "Import success";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+?>
